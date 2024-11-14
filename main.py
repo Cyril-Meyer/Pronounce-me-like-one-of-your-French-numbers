2980b7247d662176280c19a93a295408714898f2
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-11-14 23:38:59 +0100

    better results view

--- main.py
+++ main.py
@@ -3,28 +3,36 @@ from num2words import num2words
 import syllables
 
 if __name__ == "__main__":
-    for limit in range(4, 10):
-        limit = 10 ** limit
-        result = []
-        x = []
+    for t in ['length', 'syllables']:
+        for limit in range(4, 11):
+            limit = 10 ** limit
+            result = []
+            x = []
 
-        for l_id, lang in enumerate(['de', 'fr', 'en']):
-            result.append([])
-            x.append([])
+            for l_id, lang in enumerate(['de', 'fr', 'en']):
+                result.append([])
+                x.append([])
 
-            increment = int(limit / 846)  # not using 10 multiple number to avoid biases
+                increment = int(limit / 846)  # not using 10 multiple number to avoid biases
 
-            for i in range(0, limit, increment):
-                words = num2words(i, lang=lang)
-                # result[l_id].append(len(words))
+                for i in range(0, limit, increment):
+                    x[l_id].append(i)
 
-                sylls = sum(syllables.estimate(word) for word in words.split())
-                result[l_id].append(sylls + 0.25*l_id)
-                x[l_id].append(i)
+                    words = num2words(i, lang=lang)
+                    if t == 'length':
+                        result[l_id].append(len(words))
+                    else:
+                        sylls = sum(syllables.estimate(word) for word in words.split())
+                        result[l_id].append(sylls + 0.25*l_id)
 
-            plt.scatter(x[l_id], result[l_id], label=lang)
+                plt.scatter(x[l_id], result[l_id], alpha=0.2, label=lang)
 
-        plt.legend()
-        plt.title(f'[0, {limit}] (inc = {increment})')
-        plt.savefig(f'{limit}.png')
-        plt.clf()
+            if t == 'length':
+                plt.ylim(0, 120)
+            else:
+                plt.ylim(0, 40)
+
+            plt.legend()
+            plt.title(f'{t}: [0, {limit}] (inc = {increment})')
+            plt.savefig(f'{t}_{limit}.png')
+            plt.clf()
