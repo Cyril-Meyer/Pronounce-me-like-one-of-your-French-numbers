7fb6923c7fe01b2c54b076f5e0b3bd38f802f1b8
Cyril-Meyer <contact@cyrilmeyer.eu>
2024-11-14 23:27:50 +0100

    add increasing limit and increment

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import matplotlib.pyplot as plt
 from num2words import num2words
 import syllables
 
-limit = 1000
-
 if __name__ == "__main__":
-    result = []
+    for limit in range(4, 10):
+        limit = 10 ** limit
+        result = []
+        x = []
 
-    for l_id, lang in enumerate(['en', 'fr', 'de']):
-        result.append([])
+        for l_id, lang in enumerate(['de', 'fr', 'en']):
+            result.append([])
+            x.append([])
 
-        for i in range(limit):
-            words = num2words(i, lang=lang)
-            # result[l_id].append(len(words))
+            increment = int(limit / 846)  # not using 10 multiple number to avoid biases
 
-            sylls = sum(syllables.estimate(word) for word in words.split())
-            result[l_id].append(sylls)
+            for i in range(0, limit, increment):
+                words = num2words(i, lang=lang)
+                # result[l_id].append(len(words))
 
-        plt.plot(result[l_id], label=lang)
+                sylls = sum(syllables.estimate(word) for word in words.split())
+                result[l_id].append(sylls + 0.25*l_id)
+                x[l_id].append(i)
 
-    plt.legend()
-    plt.show()
+            plt.scatter(x[l_id], result[l_id], label=lang)
+
+        plt.legend()
+        plt.title(f'[0, {limit}] (inc = {increment})')
+        plt.savefig(f'{limit}.png')
+        plt.clf()
